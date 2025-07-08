095d852f12a3dab9bc8963ff275fae5944c40224
sargunsahu27 <sargunsahu73@gmail.com>
2025-07-08 10:39:32 +0530

    lecture-3 commit

---
+++ lecture-3/lecture-3-continue/index.js
@@ -0,0 +1,3 @@
+let sum=require('@sargunpreet/lecture-3/lib/sum.js')
+console.log(sum.sum(5,3))
+// readme file add krni hai (jisme saari details hongi)
---
+++ lecture-3/lecture-3-continue/node_modules/@sargunpreet/lecture-3/lib/sum.js
@@ -0,0 +1,5 @@
+// how to publish this my own built package lib to node(npm)
+function sum(a,b){
+    return a+b;
+}
+module.exports.sum=sum;
